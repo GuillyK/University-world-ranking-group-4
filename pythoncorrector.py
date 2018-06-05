ad89902152e98501bc1b064c73dd652e4284bee4
topteulen <33281957+topteulen@users.noreply.github.com>
2018-06-05 13:57:53 +0200

    cleaned university_ranking_2016 + space bar
    pythoncorrector adds spaces to name of univeristy and country wich its from

--- pythoncorrector.py
+++ pythoncorrector.py
@@ -7,16 +7,16 @@ r = pd.read_csv('university_ranking_2016.csv')
 
 df1 = pd.DataFrame(r,columns=['name'])
 df2 = pd.DataFrame(data=None, columns=df1.columns)
-l = []
+list = []
 for index, name in df1.iterrows():
-    t = re.sub(r"(\w)([A-Z])", r"\1 \2", str(name))
-    print(t)
-    l.append([t])
-    test = t.split("\t")
+    test = name["name"]
 
-    #df2.append(t)
+    t = re.sub(r"(\w)([A-Z])", r"\1 \2", test)
+    list = list + [[t]]
+    print(list)
+
 
 
 with open("out.csv", "w") as output:
     writer = csv.writer(output)
-    writer.writerows(l)
+    writer.writerows(list)
