8cc145a8361d55a79ed9cbafd9d6921b324239a0
Tobias Teule <tobiasteule@gmail.com>
2018-06-07 15:44:55 +0200

    test.py now calculates Mean of scores from country and then calcs deviation in the years to look at stability

--- week1/test.py
+++ week1/test.py
@@ -4,30 +4,33 @@ import numpy
 import matplotlib.pyplot as plt
 plt.close
 
-def scorer(university, dataset):
+def meanscorer(university, dataset):
         score = data.loc[dataset['country'] == university, ['overall']]
-        print(score)
-        score = score.iloc[0]['overall']
-        return(score)
+        return(score['overall'].mean())
 
 data = pd.read_csv("ranking_with_country_2016.csv")
-data1 = pd.read_csv("university_ranking_2017.csv")
-data2 = pd.read_csv("university_ranking_2018.csv")
+data1 = pd.read_csv("ranking_with_country_2017.csv")
+data2 = pd.read_csv("ranking_with_country_2018.csv")
 
 #for ratio in data['ratio']:
     #print(ratio)
-print(data)
-counter = 0
-counter1 = 0
-#for country in data['country']:
-#    print(country)
-    #score2016 = scorer(country, data)
-            #score2017 = scorer(university, data1)
-            #score2018 = scorer(university, data2)
-            #print(university, ' 2016=' ,score2016)
-            #print(university, ' 2017=' ,score2017)
-            #print(university, ' 2018=' ,score2018)
-    #print(score2016)
+
+countrylist = []
+
+for country in data['country']:
+    if country not in countrylist:
+        countrylist = countrylist + [country]
+
+        meancountryscore2016 = meanscorer(country, data)
+        meancountryscore2017 = meanscorer(country, data1)
+        meancountryscore2018 = meanscorer(country, data2)
+        meanlist = [meancountryscore2016] + [meancountryscore2017] + [meancountryscore2018]
+        print(country, '2016 =' ,meancountryscore2016)
+        print(country, '2017 =' ,meancountryscore2017)
+        print(country, '2018 =' ,meancountryscore2018)
+        meancountryoverall = pd.Series(meanlist)
+        deviationoverall = meancountryoverall.std()
+        print("overall deviation for", country, ' = ', deviationoverall)
 
     #print(studentsperstaff)
     #plt.scatter(studentlist, studentsperstaff)
