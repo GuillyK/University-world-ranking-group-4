2142530aa3d1bff512d3b286e8875ca33c7bdb1c
Guilly Kolkman <gkolkm@gmail.com>
2018-06-08 14:36:36 +0200

    ratio to single numbers

--- week1/nacorrecter.py
+++ week1/nacorrecter.py
@@ -2,20 +2,27 @@ import csv
 import pandas as pd
 import numpy
 
-data = pd.read_csv("ranking_with_country_2016.csv")
+data = pd.read_csv("ranking_with_country_2018.csv")
 
 #for ratio in data['ratio']:
     #print(ratio)
 
-with open("ranking_with_country_2016.csv") as out:
-    counter = 0
-    for ratio in data['ratio']:
-        print(ratio)
-        #print(type(ratio))
-        if type(ratio) == float:
-            data.set_value(counter, "ratio", None)
-        counter += 1
+# with open("ranking_with_country_2016.csv") as out:
+#     counter = 0
+#     for ratio in data['ratio']:
+#         print(ratio)
+#         #print(type(ratio))
+#         if type(ratio) == float:
+#             data.set_value(counter, "ratio", None)
+#         counter += 1
 
-    data.to_csv("ranking_with_country_2016.csv", index=False)
+#     data.to_csv("ranking_with_country_2016.csv", index=False)
 
-
+counter = 0
+for ratio in data['ratio']:
+    print(ratio)
+    if type(ratio)is not float:
+        print(ratio[:2])
+        data.set_value(counter, "ratio", ratio[:2])
+    counter+=1
+data.to_csv("ranking_with_country_2018.csv", index=False)
---
+++ week1/top200_special.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("ranking_with_country_2016.csv")
+data1 = pd.read_csv("ranking_with_country_2017.csv")
+data2 = pd.read_csv("ranking_with_country_2018.csv")
+
+# counter = 0
+# for teaching in data['teaching']:
+#     if counter < 200:
+#         print(teaching)
+#         # print(data['country']==teaching)
+#     else:
+#         break
+#     counter+=1
+teaching = []
+for i in range(200):
+    print(data['country'][i] ,data['teaching'][i])
+    if teaching == []
+        teaching.append[]
+
+# print(len(data))
