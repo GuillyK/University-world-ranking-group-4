ecce9ae1c1dde265a842b3550740b1d6258a49e8
Guilly Kolkman <gkolkm@gmail.com>
2018-06-11 13:05:08 +0200

    nan checker for all years

--- week1/nacorrecter.py
+++ week1/nacorrecter.py
@@ -18,11 +18,53 @@ data = pd.read_csv("ranking_with_country_2018.csv")
 
 #     data.to_csv("ranking_with_country_2016.csv", index=False)
 
-counter = 0
-for ratio in data['ratio']:
-    print(ratio)
-    if type(ratio)is not float:
-        print(ratio[:2])
-        data.set_value(counter, "ratio", ratio[:2])
-    counter+=1
-data.to_csv("ranking_with_country_2018.csv", index=False)
+# counter = 0
+# for ratio in data['ratio']:
+#     print(ratio)
+#     if type(ratio)is not float:
+#         print(ratio[:2])
+#         data.set_value(counter, "ratio", ratio[:2])
+#     counter+=1
+# data.to_csv("ranking_with_country_2018.csv", index=False)
+
+
+import csv
+import pandas as pd
+import numpy
+import math
+
+
+next_data = pd.read_csv('ranking_with_country_2017.csv')
+next2_data = pd.read_csv('ranking_with_country_2018.csv')
+def nan_checker(file, nan_column, output_column):
+    data = pd.read_csv(file)
+
+    with open(file) as out:
+        counter = 0
+        
+        for value in data[nan_column]:
+            counter2 = 0
+            counter3 = 0
+            if math.isnan(value):
+                uni1 = data[output_column][counter]
+                for same in next_data['name']:
+                    
+                    if same == uni1:
+                        print(same, uni1)
+                        print(next_data['ratio'][counter2])
+                        
+                    counter2 += 1
+                #print(next_data[])
+               
+                for same2 in next2_data['name']:
+                    
+                    if same2 == uni1:
+                        print(same2, uni1)
+                        print(next2_data['ratio'][counter3])
+                        
+                    counter3 += 1
+                print('--------------------------------')
+            counter += 1
+
+        
+nan_checker("ranking_with_country_2016.csv", "ratio", "name")
--- week1/nan_checker.py
+++ week1/nan_checker.py
@@ -4,15 +4,41 @@ import numpy
 import math
 
 
+next_data = pd.read_csv('ranking_with_country_2017.csv')
+next2_data = pd.read_csv('ranking_with_country_2018.csv')
 def nan_checker(file, nan_column, output_column):
     data = pd.read_csv(file)
-
+    nan_list = []
     with open(file) as out:
         counter = 0
+        
         for value in data[nan_column]:
+            counter2 = 0
+            counter3 = 0
             if math.isnan(value):
-                print(data[output_column][counter])
+                uni1 = data[output_column][counter]
+                for same in next_data['name']:
+                    
+                    if same == uni1:
+                        print(same, uni1)
+                        print(next_data['ratio'][counter2])
+                        nan = next_data['ratio'][counter2]
+                    counter2 += 1
+                #print(next_data[])
+               
+                for same2 in next2_data['name']:
+                    
+                    if same2 == uni1:
+                        print(same2, uni1)
+                        print(next2_data['ratio'][counter3])
+                        nan2 = next2_data['ratio'][counter3]
+                    counter3 += 1
+                if math.isnan(nan) and math.isnan(nan2):
+                    nan_list.append(uni1)
+                print('--------------------------------')
+            
             counter += 1
+        print(nan_list)
 
         
-nan_checker("ranking_with_country_2018.csv", "international_students", "name")
+nan_checker("ranking_with_country_2016.csv", "ratio", "name")
