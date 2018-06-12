1a2d85f45d5560e86ca4ecf2b6cd142ec926f9d1
Nienke <nienke.reints@gmail.com>
2018-06-12 12:30:16 +0200

    calculate average of column

---
+++ week1/calculate_average.py
@@ -0,0 +1,19 @@
+import csv
+import pandas as pd
+import numpy
+import math
+
+def calculate_average(file, column):
+    data = pd.read_csv(file)
+    all_ratios = []
+    for ratio in data[column]:
+        if not math.isnan(ratio):
+            all_ratios.append(ratio)
+    mean = numpy.mean(all_ratios)
+    print(file , mean)
+
+
+
+calculate_average('../DATA/ranking_with_country_2016.csv', 'ratio')
+calculate_average('../DATA/ranking_with_country_2017.csv', 'ratio')
+calculate_average('../DATA/ranking_with_country_2018.csv', 'ratio')
