5d54563ec0703c2198ff405523abd3305105a8ca
Nienke <nienke.reints@gmail.com>
2018-06-12 12:49:12 +0200

    fill in missing values

---
+++ week1/fill_missing_values.py
@@ -0,0 +1,20 @@
+import csv
+import pandas as pd
+import numpy
+import math
+
+data = pd.read_csv("../DATA/ranking_with_country_2018.csv")
+
+# for ratio in data['ratio']:
+#     print(ratio)
+
+with open("../DATA/ranking_with_country_2018.csv") as out:
+    counter = 0
+    for ratio in data['ratio']:
+        # print(ratio)
+        #print(type(ratio))
+        if math.isnan(ratio):
+            data.at[data.index[counter], 'ratio'] = 49
+        counter += 1
+
+    data.to_csv("../DATA/ranking_with_country_2018.csv", index=False)
