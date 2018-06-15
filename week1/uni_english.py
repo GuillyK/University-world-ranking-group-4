70d00b7217f7ecf028d3294c6984b9fea94cc24a
Guilly Kolkman <gkolkm@gmail.com>
2018-06-15 14:01:31 +0200

    added english speaking universities in csv

--- week1/uni_english.py
+++ week1/uni_english.py
@@ -6,7 +6,7 @@ from bokeh.layouts import row, column
 from bokeh.models import ColumnDataSource, HoverTool, LinearColorMapper
 
 
-data = pd.read_csv('../DATA/ranking_with_country_2017.csv')
+data = pd.read_csv('../DATA/ranking_with_country_2018.csv')
 
 
 english = ['Australia','Canada', 'New Zealand', 'United Kingdom', 'United States', 'Ireland']
@@ -15,4 +15,22 @@ eng_unis = []
 for country in english:
         name = data.loc[data['country'] == country, 'name'].values
         eng_unis.append(list(name))
-print(eng_unis)
+
+english_spoken = []
+for i in eng_unis:
+    for j in i:
+        english_spoken.append(j)
+#print(eng_unis)
+print(english_spoken)
+
+counter = 0
+for uni in data['name']:
+    if uni in english_spoken:
+        data['english_spoken'][counter] = '1'
+        print('1')
+    else:
+        data['english_spoken'][counter] = '0'
+        print('0')
+    counter += 1
+
+    data.to_csv("../DATA/ranking_with_country_2018.csv", index=False)
