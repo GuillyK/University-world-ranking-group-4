8124b10ae9ed814861a445b7533d365633f7159d
Nienke <nienke.reints@gmail.com>
2018-06-19 13:34:35 +0200

    add columns from other file

---
+++ week1/add_columns.py
@@ -0,0 +1,16 @@
+import csv
+import pandas as pd
+import numpy
+import math
+
+data = pd.read_csv("../DATA/ranking_with_country_2017.csv")
+data_int = pd.read_csv("../DATA/ranking_2017_with_int.csv")
+
+counter = 0
+for country in data['country']:
+    data['country_int'][counter] = data_int['country'][counter]
+    counter += 1
+
+
+
+data.to_csv("../DATA/ranking_with_country_2017.csv", index=False)
