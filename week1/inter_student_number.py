31579422173f9d97e973ed9b05a0770643cd049d
Nienke <nienke.reints@gmail.com>
2018-06-22 16:48:53 +0200

    code to draw linear regression

---
+++ week1/inter_student_number.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd
+import numpy
+
+data = pd.read_csv("../DATA/ranking_with_country_2016.csv")
+counter = 0
+for fte in data['fte_students']:
+    percentage = data['international_students'][counter]
+    international_students_number = fte*percentage
+    print(fte, percentage, international_students_number)
+    data.set_value(counter, "international_students_number", international_students_number)
+    counter += 1
+data.to_csv("../DATA/university_ranking_2016.csv", index=False)
